19eb62aecea1b0fb3484647b23afeffba878ca42
shunsuke0424 <shunsuke603ikuno@gmail.com>
2023-12-15 01:40:27 +0900

    Update type of groupID

--- api_server/domain/repository/question_repository.go
+++ api_server/domain/repository/question_repository.go
@@ -2,8 +2,9 @@ package repository
 
 import (
 	"context"
+	"pinnacle-play/domain/model"
 )
 
 type QuestionRepository interface {
-	Save(ctx context.Context, content string, groupId int) error
+	Save(ctx context.Context, content string, groupId model.GroupID) error
 }
--- api_server/domain/repository/user_repository.go
+++ api_server/domain/repository/user_repository.go
@@ -2,8 +2,9 @@ package repository
 
 import (
 	"context"
+	"pinnacle-play/domain/model"
 )
 
 type UserRepository interface {
-	Save(ctx context.Context, name string, groupId int) error
+	Save(ctx context.Context, name string, groupId model.GroupID) error
 }
