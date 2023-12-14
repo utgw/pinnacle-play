b488250141191aa93e4c976ee0b48161cf83e612
shunsuke0424 <shunsuke603ikuno@gmail.com>
2023-12-15 01:39:31 +0900

    Update GroupRepository Save Method to Return *model.Group

--- api_server/domain/repository/group_repository.go
+++ api_server/domain/repository/group_repository.go
@@ -2,8 +2,9 @@ package repository
 
 import (
 	"context"
+	"pinnacle-play/domain/model"
 )
 
 type GroupRepository interface {
-	Save(ctx context.Context, name string) error
+	Save(ctx context.Context, name string) (*model.Group, error)
 }
