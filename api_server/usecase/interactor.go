452c2731dc7c404e6e84f0e8ab7ededb153fefe7
shunsuke0424 <shunsuke603ikuno@gmail.com>
2023-12-16 20:55:48 +0900

    Add interactor.go

--- api_server/usecase/interactor.go
+++ api_server/usecase/interactor.go
@@ -1,1 +1,37 @@
 package usecase
+
+import (
+	"context"
+	"pinnacle-play/usecase/input"
+)
+
+// NewInteractor new Interactor
+func NewInteractor(
+	pre Presenter,
+	group Group,
+) Interactor {
+	return Interactor{pre, group}
+}
+
+// Interactor usecase interactor
+type Interactor struct {
+	pre  Presenter
+	group Group
+}
+
+// PostMemoAndTags save memo and tags
+func (i Interactor) PostMemoAndTags(ctx context.Context, in input.PostGroupInput) {
+	err := i.group.ValidatePost(ctx, in)
+	if err != nil {
+		i.pre.ViewError(ctx, err)
+		return
+	}
+
+	group, err := i.group.CreateGroup(ctx, in)
+	if err != nil {
+		i.pre.ViewError(ctx, err)
+		return
+	}
+
+	i.pre.ViewGroup(ctx, group)
+}
