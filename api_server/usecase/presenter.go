33f4ab46ec2e58c4f5d91f581b552a13fef00b1a
shunsuke0424 <shunsuke603ikuno@gmail.com>
2023-12-16 20:56:30 +0900

    Add presenter.go

--- api_server/usecase/presenter.go
+++ api_server/usecase/presenter.go
@@ -1,1 +1,12 @@
 package usecase
+
+import (
+	"context"
+	"pinnacle-play/domain/model"
+)
+
+// Presenter presenter interface
+type Presenter interface {
+	ViewError(ctx context.Context, err error)
+	ViewGroup(ctx context.Context, mg *model.Group)
+}
