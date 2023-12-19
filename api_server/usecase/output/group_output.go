259e7a63d7801f509a4e60f00ccdb0529bf19f79
shunsuke0424 <shunsuke603ikuno@gmail.com>
2023-12-20 00:49:08 +0900

    change: Update PostGroupOutput type

--- api_server/usecase/output/group_output.go
+++ api_server/usecase/output/group_output.go
@@ -4,6 +4,6 @@ import "pinnacle-play/domain/model"
 
 type PostGroupOutput struct {
 	Group     *model.Group
-	Users     *model.Users
-	Questions *model.Questions
+	Users     []*model.User
+	Questions []*model.Question
 }
