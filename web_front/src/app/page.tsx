dd33f49d2cfb9f4ac488f8c2adaea61464a5a8d6
utgw <utgw.1202@gmail.com>
2023-12-20 23:37:58 +0900

    Install prettier and eslint

--- web_front/src/app/page.tsx
+++ web_front/src/app/page.tsx
@@ -1,9 +1,32 @@
-import { Button } from "@mui/material";
+import { Box, Button, Container, Typography } from "@mui/material";
 
 export default function Home() {
   return (
-    <main>
-      <Button variant="contained" >始める</Button>
-    </main>
-  )
+    <Container
+      sx={{ display: "flex", flexDirection: "column", alignItems: "center" }}
+    >
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          marginBottom: "20px",
+        }}
+      >
+        <Typography variant="h2">Pinnacle Play</Typography>
+      </Box>
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+          width: "600px",
+          alignItems: "center",
+        }}
+      >
+        <Box>ユーザー名</Box>
+        <Box>グループ作成</Box>
+        <Box>...</Box>
+      </Box>
+    </Container>
+  );
 }
