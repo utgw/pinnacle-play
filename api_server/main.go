b40bf428bf451eebb772a8eb4a16891aeb06b5ab
shunsuke0424 <shunsuke603ikuno@gmail.com>
2023-12-22 00:52:19 +0900

    fix: add error handling for server startup failure

--- api_server/main.go
+++ api_server/main.go
@@ -1,13 +1,15 @@
 package main
 
 import (
+	"log"
+
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	//Ginフレームワークのデフォルトの設定を使用してルータを作成
 	router := gin.Default()
-	
+
 	// ルートハンドラの定義
 	router.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
@@ -16,5 +18,7 @@ func main() {
 	})
 
 	// サーバー起動
-	router.Run(":3000")
+	if err := router.Run(":3000"); err != nil {
+		log.Fatalf("Failed to run server: %v", err)
+	}
 }
