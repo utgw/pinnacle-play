d33769a4131c6b67830066ac3335579fbb69ae84
shunsuke0424 <shunsuke603ikuno@gmail.com>
2024-01-13 16:10:15 +0900

    change: delete usecase

--- api_server/usecase/input/group_input.go
+++
@@ -1,9 +0,0 @@
-package input
-
-import "pinnacle-play/domain/model"
-
-type PostGroupInput struct {
-	GroupName        model.GroupName
-	UserNames        []model.UserName
-	QuestionContents []model.QuestionContent
-}
